f7c2ab9039130be131ff6a49c53f638efe13834d
Sven Laater <sven.laater@gmail.com>
2019-01-18 23:29:07 +0700

    bump dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,10 @@
   :url "https://github.com/svenlaater/clojure-graphql-api.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.stuartsierra/component "0.3.2"]
-                 ;;[com.walmartlabs/lacinia "0.30.0"]
-                 [com.walmartlabs/lacinia-pedestal "0.5.0"]
-                 [io.aviso/logging "0.2.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [com.stuartsierra/component "0.4.0"]
+                 [com.walmartlabs/lacinia-pedestal "0.11.0"]
+                 [io.aviso/logging "0.3.1"]
                  ;; fix issue w/ jdk-11
                  ;; java.lang.IllegalArgumentException: Must hint overloaded method: toArray,
                  ;; compiling:(flatland/ordered/set.clj:19:1)
