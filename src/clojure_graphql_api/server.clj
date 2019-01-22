e778eb527f4249cd06f9cea2d50a719dd0fb3340
Sven Laater <sven.laater@gmail.com>
2019-01-22 17:44:51 +0700

    fix CORS

--- src/clojure_graphql_api/server.clj
+++ src/clojure_graphql_api/server.clj
@@ -1,7 +1,8 @@
 (ns clojure-graphql-api.server
   (:require [com.stuartsierra.component :as component]
             [com.walmartlabs.lacinia.pedestal :as lp]
-            [io.pedestal.http :as http]))
+            [io.pedestal.http :as http]
+            [io.pedestal.http.cors :as cors]))
 
 (defrecord Server [schema-provider server]
 
@@ -10,6 +11,8 @@
     (assoc this :server (-> schema-provider
                             :schema
                             (lp/service-map {:graphiql true})
+                            (merge { ::http/allowed-origins {:creds true :allowed-origins (constantly true)}})
+                            http/default-interceptors
                             http/create-server
                             http/start)))
 
