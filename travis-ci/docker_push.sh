ca4a8d88001bb4cb46ea49d0c6041c7577eac089
Sven Laater <sven.laater@gmail.com>
2019-02-10 15:07:38 +0800

    make things nicer to read and use

--- travis-ci/docker_push.sh
+++ travis-ci/docker_push.sh
@@ -6,7 +6,7 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS_BRANCH" == "master" ] ; then
         # Make sure to have AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
         # defined as environment variables
-        eval $(aws ecr get-login --region eu-central-1 --no-include-email | sed 's|https://||')
-        docker push ${ECR}/${NAME}:${TAG}
+        eval $(aws ecr get-login --region ${REGION} --no-include-email | sed 's|https://||')
+        docker push $IMAGE
     fi
 fi
